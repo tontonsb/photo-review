fabdf83d51d349c35d7f3f983bf5f36a2fd97dec
Džuris <juris@glaive.pro>
2024-05-10 11:18:17 +0300

    Style desktop UI

--- resources/views/random.blade.php
+++ resources/views/random.blade.php
@@ -1,31 +1,35 @@
 @extends('base-layout')
 
 @section('body')
-<a href="{{$file->url}}" target="_blank">
-    <img src="{{$file->url}}">
-</a>
+<main>
+    <a href="{{$file->url}}" target="_blank">
+        <img src="{{$file->url}}">
+    </a>
 
-<form method=post action="{{route('reviews.store')}}">
-    @csrf
-    <input type=hidden name=filepath value="{{$file->path}}">
-    <input type=hidden name=reviewing_duration_ms value=0>
+    <form method=post action="{{route('reviews.store')}}">
+        @csrf
+        <input type=hidden name=filepath value="{{$file->path}}">
+        <input type=hidden name=reviewing_duration_ms value=0>
 
-    <button type=submit name=conclusion value=ok class=button--ok>Apskatīju, nav nekā ievērības cienīga</button>
+        <button type=submit name=conclusion value=ok class=button--ok>Apskatīju, nav nekā ievērības cienīga</button>
 
-    <details open>
-        <summary>Redzi ko aizdomīgu?</summary>
-        <textarea name=review></textarea>
-    </details>
+        <div>
+            <details open>
+                <summary>Redzi ko aizdomīgu?</summary>
+                <textarea name=review></textarea>
+            </details>
 
-    <details>
-        <summary>Bilde ir nekvalitatīva vai cita problēma?</summary>
-        <textarea name=problem></textarea>
-    </details>
+            <details>
+                <summary>Bilde ir nekvalitatīva vai cita problēma?</summary>
+                <textarea name=problem></textarea>
+            </details>
 
-    <button type=submit name=conclusion value=suspect class=button--suspect>Iesniegt</button>
+            <button type=submit name=conclusion value=suspect class=button--suspect>Iesniegt</button>
+        </div>
 
-    <button type=submit name=conclusion value=skip class=button--skip>Izlaist šo bildi</button>
-</form>
+        <button type=submit name=conclusion value=skip class=button--skip>Izlaist šo bildi</button>
+    </form>
+</main>
 
 <script>
 const timeStarted = new Date()
