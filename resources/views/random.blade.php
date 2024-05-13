4b71d1888c19079fcc8eb9df46102d7fa5503724
Džuris <juris@glaive.pro>
2024-05-13 19:15:14 +0300

    Add infobox/tutorial

--- resources/views/random.blade.php
+++ resources/views/random.blade.php
@@ -77,10 +77,80 @@
                 </span>
             @endif
 
-            <button type=submit name=conclusion value=skip class=button--skip>Izlaist šo bildi</button>
+            <button type=submit name=conclusion value=skip class=button--info>Izlaist šo bildi</button>
+
+            <button type=button class="js-show-infobox button--info">Atvērt pamācību</button>
         </div>
     </form>
 </main>
+
+<dialog class=js-infobox>
+    <div>
+        <button title=Aizvērt class="x-button js-close">✕</button>
+
+        <div autofocus>
+            <p>Šī projekta mērķis ir iegūt informāciju par to, kas notics ar bezvēsts pazudušo Annu Jansoni.
+
+            <p>Anna pazuda 2023. gada 1. novembrī un par pēdējo zināmo viņas atrašanās vietu uzskatām meža ceļu pie šosejas P101 5.5 km atzīmes.
+            Kopš 2. novembra nav iegūta jauna informācija par to, kas ar Annu noticis vai kurā virzienā viņa devusies.
+            Visticamāk, viņai līdzi bija pelēks pusmētelis, melni puszābaki un melnas džinsu bikses. Iespējams, arī mugursoma ar sarkaniem akcentiem un varbūt antifrīza pudele viena litra tilpumā.
+
+            <p><strong>Lūdzu rūpīgi apskatīt bildes un ierakstīt informāciju, ja kādā bildē redzams jebkas, kas varētu liecināt par to, kas ar Annu noticis, kurā virzienā viņa devusies.</strong>
+
+            <details>
+                <summary>
+                    Kas tās par bildēm? Kā tās skatīties?
+                </summary>
+
+                <p>Attēli ir fotogrāfijas un eholotes datu ieraksti (atpazīstami pēc oranžajiem toņiem) no pazušanas vietas apkārtnes.
+
+                <p>Fotogrāfijas ir uzņemtas no gaisa, augstumā līdz 70 metriem. Tās var būt stipri jāpievelk, lai aplūkotu visas detaļas.
+                Bildes pietuvināt var ar peles rullīti vai pirkstu atdalīšanas žestu uz skārienekrāniem un skārienpaliktņiem. Pēc pietuvināšanas bildi var bīdīt ar kursoru vai pirkstu (skārienekrānos).
+
+                <p>Informācijas un pogu joslas sākumā ir attēla nosaukums, uz kuru nospiežot var atvērt papildus informāciju par šo bildi — tiešo saiti uz bildi, atrašanās vietu un dažādus tehniskos datus.
+            </details>
+
+            <details>
+                <summary>Kas ir tās oranžās bildes?</summary>
+
+                <p>Eholotes ieraksti attēlo ainu ūdenskrātuvēs zem ūdens. Mēroga izpratnei der zināt, ka joslas platums ir 10–50 metri, parasti 20–40.
+                Attēlojumi mēdz būt divu veidu: oriģinālie ar atšķirīgu joslu vidū un SRC (slant range corrected) bez tās.
+
+
+                <p>Oriģinālajos ģeometrija ir deformēta — objektu izmēri joslas virzienā (paralēli laivas braukšanas virzienam) ir korekti, bet prependikulāri
+                tam izmēri ir saspiesti tā, turklāt joslas centram tuvākās attēla daļas ir saspiestas vairāk nekā tālākie.
+                SRC šī ģeometrija ir izlabota, bet bieži tas noved pie attēla kvalitātes zuduma.
+            </details>
+
+            <details>
+                <summary>
+                    Kuros gadījumos izmantot kuras pogas?
+                </summary>
+
+                <p>Ja bildē redzams jebkas, kas sniedz informāciju par to, kas varētu būt noticis ar Annu vai kur viņa varētu būt devusies, to jāieraksta lodziņā
+                "Redzi ko aizdomīgu?" un jāspiež pogu "Iesniegt". Tieši tāpat jārīkojas arī gadījumos, kad nav pārliecības un šķiet, ka attēlā redzamo vajadzētu pārbaudīt klātienē —
+                piemēram, ja bildē redzama ēka, kurā būtu vērts ielūkoties.
+
+                <p>Gadījumos, kad aplūkojamā vieta nav redzama gana labi un nepieciešams par to vietu gatavot jaunu bildi, lūdzu problēmu norādīt lodziņā, kurš atveras spiežot "Bilde ir nekvalitatīva vai cita problēma?" un iesniegt.
+                Tas varētu būt ne tikai burtiskā izplūdušas bildes gadījumā, bet arī, piemēram, tad, ja eholotes ierakstā josla būtiski nosedz citu joslas daļu. Pie vienas bildes drīkst aizpildīt abus teksta lodziņus.
+
+                <p>Gadījumā, ja redzams, ka bildē nav nekā, kas varētu sniegt šādu informāciju, jāspiež zaļo pogu "Apskatīju, nav nekā ievērības cienīga".
+
+                <p>Sistēma centīsies atcerēties aplūkotās bildes un vienam cilvēkam vienā pārlūkā atkārtoti nerādīt jau pārskatīto.
+
+                <p>Ja šajā brīdī kāda bilde šķiet par sarežģītu, lai to kārtīgi pārbaudīt, vari ekrāna apakšā spiest "Izlaist šo bildi".
+                Izlaistās bildes parādīs atkārtoti, ja visas pārējās būs jau apskatītas.
+            </details>
+
+
+            <p>Ar meklēšanu saistītos ģeodatus — pārmeklētās vietas, novērotos objektus u.c. var aplūkot <a href="https://ej.uz/AnnasVietas" target=_blank>ej.uz/AnnasVietas</a>.
+
+            <p>Jebkuru citu informācija par to, kas ar Annu varētu būt noticis lūdzu sniegt
+            uz <a href="tel:27020337">27020337</a> (pieejams arī WhatsApp, Signal, Telegram),
+             <a href="mailto:juris@glaive.pro">juris@glaive.pro</a> vai Valsts policijai.
+        </div>
+    </div>
+</dialog>
 
 <script type=module>
 import Zoomist from 'https://cdn.jsdelivr.net/npm/zoomist@2/zoomist.js'
@@ -113,6 +183,13 @@ zoomistOverlay.addEventListener('mouseup', () => {
 
     zoomistOverlay.style.cursor = 'grab'
 })
+
+const infobox = document.querySelector('.js-infobox')
+infobox.showModal()
+infobox.querySelectorAll('.js-close').forEach(
+    button => button.addEventListener('click', _ => infobox.close())
+)
+document.querySelector('.js-show-infobox').addEventListener('click', _ => infobox.showModal())
 
 const timeStarted = new Date()
 const form = document.querySelector('form')
