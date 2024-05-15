996cc38edf8b88e6effcaf4f26eb3f126c6b0e46
Džuris <juris@glaive.pro>
2024-05-15 19:13:24 +0300

    Update metadata

--- resources/views/base-layout.blade.php
+++ resources/views/base-layout.blade.php
@@ -4,18 +4,19 @@
 <meta name=viewport content="width=device-width, initial-scale=1">
 @vite(['resources/css/app.css'])
 
-<title>Attēlu pārskatītājs</title>
-<meta name=title content="Attēlu pārskatītājs">
+<title>Attēlu pārskatīšana Annas meklēšanai</title>
+<meta name=title content="Attēlu pārskatīšana Annas meklēšanai">
 <meta name=description content="Pārskatām apkārtnes bildes, lai noskaidrotu, kas un kur noticis ar Annu">
 <meta property=og:type content=website>
 <meta property=og:url content="{{url()->full()}}">
-<meta property=og:title content="Attēlu pārskatītājs">
+<meta property=og:title content="Attēlu pārskatīšana Annas meklēšanai">
 <meta property=og:description content="Pārskatām apkārtnes bildes, lai noskaidrotu, kas un kur noticis ar Annu">
 <meta property=og:image content="{{url('card.jpg')}}">
+<meta property=og:locale content=lv_LV>
 
 <meta name=twitter:card content=summary_large_image>
 <meta name=twitter:url content="{{url()->full()}}">
-<meta name=twitter:title content="Attēlu pārskatītājs Annas meklēšanai">
+<meta name=twitter:title content="Attēlu pārskatīšana Annas meklēšanai">
 <meta name=twitter:description content="Pārskatām apkārtnes bildes, lai noskaidrotu, kas un kur noticis ar Annu">
 <meta name=twitter:image content="{{url('card.jpg')}}">
 
