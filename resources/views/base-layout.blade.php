61330920b00ebb58222b7c39366ec2325212a90c
Džuris <juris@glaive.pro>
2024-05-15 17:59:55 +0300

    Add more metadaa

--- resources/views/base-layout.blade.php
+++ resources/views/base-layout.blade.php
@@ -8,19 +8,15 @@
 <meta name=title content="Attēlu pārskatītājs">
 <meta name=description content="Pārskatām apkārtnes bildes, lai noskaidrotu, kas un kur noticis ar Annu">
 <meta property=og:type content=website>
-{{--
 <meta property=og:url content="{{url()->full()}}">
 <meta property=og:title content="Attēlu pārskatītājs">
 <meta property=og:description content="Pārskatām apkārtnes bildes, lai noskaidrotu, kas un kur noticis ar Annu">
---}}
 <meta property=og:image content="{{url('card.jpg')}}">
 
 <meta name=twitter:card content=summary_large_image>
-{{--
 <meta name=twitter:url content="{{url()->full()}}">
-<meta name=twitter:title content="Attēlu pārskatītājs">
+<meta name=twitter:title content="Attēlu pārskatītājs Annas meklēšanai">
 <meta name=twitter:description content="Pārskatām apkārtnes bildes, lai noskaidrotu, kas un kur noticis ar Annu">
---}}
 <meta name=twitter:image content="{{url('card.jpg')}}">
 
 <link rel=apple-touch-icon sizes=180x180 href=/apple-touch-icon.png>
