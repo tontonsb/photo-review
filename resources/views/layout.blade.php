e2a41004e02eafc63ed5e9f3f93b0d978369af81
Džuris <juris@glaive.pro>
2024-05-17 22:26:40 +0300

    Add link

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
         <li><a href="{{route('reviewables.index')}}">Bildes</a>
         <li><a href="{{route('reviewers.index')}}">Pārskatītāji</a>
         <li><a href="{{route('reviewers.show', ReviewerService::getCurrentToken())}}">Tavi pārskatījumi</a>
+        <li><a href="{{route('reviewables.random')}}">Turpināt pārskatīšanu</a>
     </ul>
 </nav>
 
