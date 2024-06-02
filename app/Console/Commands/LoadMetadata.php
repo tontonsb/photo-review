6c993367cf7436ce4256b490cd76d6a28126e71c
Džuris <juris@glaive.pro>
2024-06-02 20:50:08 +0300

    Add loading option

--- app/Console/Commands/LoadMetadata.php
+++ app/Console/Commands/LoadMetadata.php
@@ -8,15 +8,19 @@ use Illuminate\Console\Command;
 class LoadMetadata extends Command
 {
     protected $signature = 'app:load-metadata
-                            {--all : Reload metadata for all images}';
+                            {--all : Reload metadata for all images}
+                            {--missing-loc : Reload metadata for images without locations}';
 
     protected $description = 'Load missing metadata';
 
     public function handle()
     {
-        $reviewables = $this->option('all')
+        $reviewables = $this->option('all') || $this->option('missing-loc')
             ? Reviewable::all()
             : Reviewable::whereNull('metadata')->get();
+
+        if ($this->option('missing-loc'))
+            $reviewables = $reviewables->filter(fn($r) => !isset($r->metadata['LOCATION']));
 
         $this->withProgressBar($reviewables, $this->setMetadata(...));
     }
