1bc4a9ae1abc8298dc200c6ede9335e289fd5210
Džuris <juris@glaive.pro>
2024-06-21 19:03:13 +0300

    Describe registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,4 +38,17 @@
 
     <button type=submit>Reģistrēties</button>
 </form>
+
+<article>
+    <h3>Kāpēc reģistrēties?</h3>
+    <p>Pēc piereģistrēšanās varēsi savam kontam piesaistīt pārskatīšanas progresu. Tas nodrošinās, ka to nepazaudēsi arī notīrot sīkdatnes vai mainot pārlūku vai ierīci.
+    <p>Tas ir izdevīgi arī tad, ja pārskatīšanu veic uz vairākām ierīcēm — progress skaitīsies kopīgi un vienā ierīce izskatītais netiks rādīts citā.
+
+    <h3>Kas notiks ar iesniegtajiem datiem?</h3>
+    <p>Lietotnes turētājiem ir redzami norādītie lietotāju vārdi un epasti.
+    Epasts nepieciešamības gadījumā var tikt izmantots, lai sazinātos ar pārskatītāju par kādu noteiktu pārskatījumu.
+    Paroles pirms saglabāšanas tiek apstrādātas ar <a href="https://en.wikipedia.org/wiki/Cryptographic_hash_function">jaucējfunkciju</a> un tiek saglabāta tikai jaucējvērtība, pēc kuras nevar uzzināt norādīto paroli.
+
+    <p>Nekādām reklāmu vajadzībām ievadītā informācija izmantota netiks, kā arī tā netiks publicēta vai nodota citām personām, izņemot gadījumus, kad to darīt liks likums.
+</article>
 @endsection
