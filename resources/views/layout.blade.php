a44b83e6e626b40a0340b8bfe37fc6c69eaef3fc
Džuris <juris@glaive.pro>
2024-06-21 19:28:19 +0300

    Add footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,31 +8,45 @@
 @endsection
 
 @section('body')
-<nav>
-    <ul>
-        <li><a href="{{ route('reviews.index') }}" class="{{ request()->routeIs('reviews.index') ? 'active' : '' }}">Visi pārskatījumi</a></li>
-        <li><a href="{{ route('reviewables.index') }}" class="{{ request()->routeIs('reviewables.index') ? 'active' : '' }}">Bildes</a></li>
-        <li><a href="{{ route('reviewers.index') }}" class="{{ request()->routeIs('reviewers.index') ? 'active' : '' }}">Pārskatītāji</a></li>
-        <li><a href="{{ route('reviewers.me') }}" class="{{ request()->routeIs('reviewers.me') ? 'active' : '' }}">Tavi pārskatījumi</a></li>
-        <li><a href="{{ route('reviewables.random') }}" class="{{ request()->routeIs('reviewables.random') ? 'active' : '' }}">Turpināt pārskatīšanu</a></li>
-    </ul>
+<header>
+    <nav>
+        <ul>
+            <li><a href="{{ route('reviews.index') }}" class="{{ request()->routeIs('reviews.index') ? 'active' : '' }}">Visi pārskatījumi</a></li>
+            <li><a href="{{ route('reviewables.index') }}" class="{{ request()->routeIs('reviewables.index') ? 'active' : '' }}">Bildes</a></li>
+            <li><a href="{{ route('reviewers.index') }}" class="{{ request()->routeIs('reviewers.index') ? 'active' : '' }}">Pārskatītāji</a></li>
+            <li><a href="{{ route('reviewers.me') }}" class="{{ request()->routeIs('reviewers.me') ? 'active' : '' }}">Tavi pārskatījumi</a></li>
+            <li><a href="{{ route('reviewables.random') }}" class="{{ request()->routeIs('reviewables.random') ? 'active' : '' }}">Turpināt pārskatīšanu</a></li>
+        </ul>
 
-    @auth
-        <form method=post action="{{route('logout')}}">
-            @csrf
-            <p>
-                <a href="{{route('me')}}">{{auth()->user()->name}}</a>
-                (<a
-                    href="{{route('logout')}}"
-                    onclick="event.preventDefault();this.closest('form').submit();"
-                >Atslēgties</a>)
-</p>
-        </form>
-    @else
-        <a href="{{route('login')}}">Pieslēgties</a>
-    @endauth
-</nav>
+        @auth
+            <form method=post action="{{route('logout')}}">
+                @csrf
+                <p>
+                    <a href="{{route('me')}}">{{auth()->user()->name}}</a>
+                    (<a
+                        href="{{route('logout')}}"
+                        onclick="event.preventDefault();this.closest('form').submit();"
+                    >Atslēgties</a>)
+                </p>
+            </form>
+        @else
+            <a href="{{route('login')}}">Pieslēgties</a>
+        @endauth
+    </nav>
+</header>
 
-@yield('content')
+<main>
+    @yield('content')
+</main>
+
+<footer>
+    <hr>
+    <nav>
+        <p>
+            Sazināties ar projekta turētāju — <a href="mailto:juris@glaive.pro">juris@glaive.pro</a>
+        </p>
+        <a href="https://github.com/tontonsb/photo-review/" target=_blank>Projekta izejas kods</a>
+    </nav>
+</footer>
 
 @endsection
