97f73c87ca9a5cec00c78193006e73beeaca746d
Džuris <juris@glaive.pro>
2024-06-22 07:25:49 +0300

    Add tracking

--- resources/views/base-layout.blade.php
+++ resources/views/base-layout.blade.php
@@ -26,4 +26,21 @@
 
 @yield('head')
 
+@if(config('services.matomo.url'))
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  _paq.push(['disableCookies']);
+  (function() {
+    var u="{{config('services.matomo.url')}}";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '{{config('services.matomo.site_id')}}']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+@endif
+
 @yield('body')
