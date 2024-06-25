f8f3017c68b44cf364311f77134b2624a757e204
Džuris <juris@glaive.pro>
2024-06-25 07:26:19 +0300

    Fix GeoJsonCollection

--- app/Http/Resources/GeoJsonCollection.php
+++ app/Http/Resources/GeoJsonCollection.php
@@ -3,9 +3,9 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
-class GeoJsonCollection extends ResourceCollection
+class GeoJsonCollection extends AnonymousResourceCollection
 {
     public static $wrap = 'features';
 
