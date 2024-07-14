0f862a8f3881f091d23d6d033bceb3aeef9ae66e
Džuris <juris@glaive.pro>
2024-07-14 21:49:48 +0300

    Cache GeoJSON

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function(Middleware $middleware) {
         $middleware->appendToGroup('web', AddReviewerToken::class);
+        $middleware->alias([
+            'cacheResponse' => \Spatie\ResponseCache\Middlewares\CacheResponse::class,
+        ]);
     })
     ->withExceptions(function(Exceptions $exceptions) {
         //
